cfca8993a4cd0c24be16fa11bcbf6404128f6406
DominikSucharski <dominik1900@o2.pl>
2020-04-06 14:32:35 +0200

    .

---
+++ 3DES/des.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DES
+{
+    class des
+    {
+        public string Encrypt()
+        {
+            return "";
+        }
+
+        public string Decrypt()
+        {
+            return "";
+        }
+
+
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Implementacja algorytmu 3DES w języku C#
+## Dominik Sucharski 131146
